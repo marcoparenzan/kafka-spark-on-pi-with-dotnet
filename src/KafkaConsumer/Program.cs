2616a7ff171e26c0015338c65e522995f3954c48
Marco Parenzan <marco.parenzan@1nn0va.it>
2021-09-16 23:27:09 +0200

    First commit

---
+++ src/KafkaConsumer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+var conf = new ConsumerConfig
+{
+    BootstrapServers = "edge01",
+    GroupId = "cons-demo",
+    BrokerAddressFamily = BrokerAddressFamily.V6
+};
+
+using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+{
+    c.Subscribe("test01");
+
+    CancellationTokenSource cts = new CancellationTokenSource();
+    Console.CancelKeyPress += (_, e) => {
+        e.Cancel = true; // prevent the process from terminating.
+        cts.Cancel();
+    };
+
+    try
+    {
+        while (true)
+        {
+            try
+            {
+                var cr = c.Consume();
+                Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+            }
+            catch (ConsumeException e)
+            {
+                Console.WriteLine($"Error occured: {e.Error.Reason}");
+            }
+        }
+    }
+    catch (OperationCanceledException)
+    {
+        // Ensure the consumer leaves the group cleanly and final offsets are committed.
+        c.Close();
+    }
+}
---
+++ src/KafkaProducer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace KafkaProducer
+{
+    static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig { 
+                BootstrapServers = "edge01",
+                //SaslUsername = "prod1",
+                //SaslPassword = "prod1-secret",
+                //SaslMechanism = SaslMechanism.ScramSha256,
+                //SecurityProtocol = SecurityProtocol.SaslSsl,
+                //EnableSslCertificateVerification = false,
+                //Debug = "broker,topic,msg",
+                //Acks = Acks.None,
+                //CompressionType = CompressionType.None
+            };
+
+            // If serializers are not specified, default serializers from
+            // `Confluent.Kafka.Serializers` will be automatically used where
+            // available. Note: by default strings are encoded as UTF8.
+            using (var p = new ProducerBuilder<string, string>(config).Build())
+            {
+                while (true)
+                {
+                    var dr = await p.ProduceAsync("test01", new Message<string, string> { Key = "a0001", Value = $"{DateTime.Now}" });
+                    Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                    await Task.Delay(1000);
+                }
+            }
+        }
+    }
+}
