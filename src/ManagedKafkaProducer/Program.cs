26702fe2edaccec5908de1adccfb0a913586ede5
Marco Parenzan <marco.parenzan@1nn0va.it>
2021-09-23 22:36:52 +0200

    ManagerKafkaProducer seems working

--- src/ManagedKafkaProducer/Program.cs
+++ src/ManagedKafkaProducer/Program.cs
@@ -7,10 +7,13 @@ var logger = new Kafka.Public.Loggers.ConsoleLogger();
 
 var cluster = new ClusterClient(new Configuration { Seeds = "edge01:9092" }, logger);
 
+var random = new Random();
+
 while (true)
 {
-    var msg = $"{DateTime.Now}";
-    cluster.Produce("test01", Encoding.UTF8.GetBytes(msg));
-    Console.WriteLine(msg);
-    await Task.Delay(1000);
+    var word = random.Next(1, 10).ToString();
+    var now = DateTime.UtcNow;
+    cluster.Produce("test01", $"{now.ToString("u")}", $"{word}", now);
+    Console.WriteLine(word);
+    await Task.Delay(100);
 }
